e045230a2792be737304ce059c92661fbab2bf6d
Dwight Spencer <dspencer@computekindustries.com>
2026-05-26 01:28:41 -0400

    Fix syntax error in build.sh for QR code generation

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 QROPTS="--verbose --background=00000000 --foreground=FFFFFFFF"
 
-xargs -a wallets.txt -P4 -n1 -- /usr/bin/env QROPTS=$QROPTS sh -c '
+xargs -a wallets.txt -P4 -n1 -- /usr/bin/env QROPTS="$QROPTS" sh -c '
     label="${1%%:*}"; address="${1#*:}";
     qrencode -t SVG --svg-path-width=1024 $QROPTS -o "$label.svg" "$address"
     qrencode -s 10 $QROPTS -o "$label.png" "$address"
